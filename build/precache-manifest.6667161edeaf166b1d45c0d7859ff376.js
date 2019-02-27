81cbb5d16cf63addaa0dc52c1a1603eaebfffab9
danfred360 <dannyfrederick360@gmail.com>
2019-02-26 20:38:05 -0500

    wr

---
+++ build/precache-manifest.6667161edeaf166b1d45c0d7859ff376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85e3bf223889503391a1",
+    "url": "/contact-manager/static/js/runtime~main.85e3bf22.js"
+  },
+  {
+    "revision": "c99b810b023efd93d7dd",
+    "url": "/contact-manager/static/js/main.c99b810b.chunk.js"
+  },
+  {
+    "revision": "f4b1d792f532aea5b162",
+    "url": "/contact-manager/static/js/2.f4b1d792.chunk.js"
+  },
+  {
+    "revision": "c99b810b023efd93d7dd",
+    "url": "/contact-manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "f4b1d792f532aea5b162",
+    "url": "/contact-manager/static/css/2.70e51e55.chunk.css"
+  },
+  {
+    "revision": "e064ff7c538c79dca585c345a48bf6da",
+    "url": "/contact-manager/index.html"
+  }
+];
